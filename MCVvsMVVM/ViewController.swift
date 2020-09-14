1d13f0b12e6b7e021ced589019e2214df168b0b1
Alex Nagy <therebeloper@gmail.com>
2020-09-14 10:37:57 +0300

    Initial Commit

---
+++ MCVvsMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MCVvsMVVM
+//
+//  Created by Alex Nagy on 14/09/2020.
+//  Copyright © 2020 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
